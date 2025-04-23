7505474c04e18909aab64497da339a9630f1263f
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-04-23 19:04:44 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:3000" || "https://web-site-create-website-backend.vercel.app"
+export const BACKEND_URL =  "https://web-site-create-website-backend.vercel.app"
